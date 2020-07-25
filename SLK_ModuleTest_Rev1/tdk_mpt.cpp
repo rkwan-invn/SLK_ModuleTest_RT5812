05f9bb0d44c4bc61409539f4309a623b461c244e
Robert Kwan <rkwan@invcorp.invensense.com>
2020-07-24 18:40:30 -0700

    7/24/2020 NP Rev 1.19 Changes in this release are:- 1.     Update test stations 4 to 5 cfg files but sync up all cfg files to rev13

--- SLK_ModuleTest_Rev1/tdk_mpt.cpp
+++ SLK_ModuleTest_Rev1/tdk_mpt.cpp
@@ -14,7 +14,7 @@
 
  // SLK MT SW version 
 #define SLKMTSWVERSIONMAJOR	1
-#define SLKMTSWVERSIONMINOR	18
+#define SLKMTSWVERSIONMINOR	19
 
 //#include "resource.h"
 //#pragma comment(lib, "user32")
